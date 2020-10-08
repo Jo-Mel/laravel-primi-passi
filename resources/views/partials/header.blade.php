2633d573705aa1abb02d34fe28599167ed421ee4
Jona M <jonameleqi@gmail.com>
2020-10-08 19:49:59 +0200

    pagina prodotti

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <img
+        src="https://cdn.shortpixel.ai/client/q_lossy,ret_img/https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png"
+        alt="logo"
+    />
+    <nav>
+        <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('prodotti') }}">Prodotti</a></li>
+            <li><a href="{{ route('contatti') }}">Contatti</a></li>
+        </ul>
+    </nav>
+</header>
