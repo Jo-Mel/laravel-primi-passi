f512e5b2fffed849f8c696bd2627c35f2db9452b
Jona M <jonameleqi@gmail.com>
2020-10-12 09:27:21 +0200

    ok

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+@section('titolo', 'contatti')
+@section('main')
+    <div class="info">
+        <ul>
+            <li>Ragione Sociale: LA MOLISANA S.P.A.</li>
+            <li>Sede Legale: Contrada Colle Delle Api, 100/A - 86100 - Campobasso (CB)</li>
+            <li>PEC: LAMOLISANA@PEC.IT</li>
+            <li>Tel: +39 0874 4981</li>
+            <li>Fax: +39 0874 629584</li>
+            <li>Per sefnalazioni scrivere a: INFO@LAMOLISANA.IT</li>
+            <li>Per contattare l'area commerciale: COMMERCIALE@LAMOLISANA.IT</li>
+            <li>Per contattare l'area export: EXPORT@LAMOLISANA.IT</li>
+            <li>Dati aziendali: <a href="#">LEGGI DI PIÙ</a></li>
+        </ul>
+    </div>
+
+
+@endsection
+    
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('titolo', 'home')
+@section('main')
+    <div class="homepg">
+        
+        <div class="desc">
+            <p>Questa non è semplicemente una pasta, ma è la storia di una Famiglia che da 4 generazioni e più di un secolo, seleziona e macina solo i grani più pregiati e di una terra che per le sue caratteristiche oggettive e irripetibili è il luogo ideale per dare alla luce una pasta squisita e unica.</p>
+            
+        </div>
+
+    </div>
+@endsection
